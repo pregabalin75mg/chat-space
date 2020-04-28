d5e7668c17fec3a7ae19c2a38dec36277d9712a0
pregabalin75mg <tsubasa1117@mbr.nifty.com>
2020-04-28 12:42:05 +0900

    Move validates over "end"

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :name, :email, presence: true, uniqueness: true
 end
-validates :name, :email, presence: true, uniqueness: true
