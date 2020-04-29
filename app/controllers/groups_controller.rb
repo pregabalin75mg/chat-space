9522e8ac750b9bee1702bd315f60b9b3b61bf72e
pregabalin75mg <tsubasa1117@mbr.nifty.com>
2020-04-29 13:59:55 +0900

    Edted groups_controller
    edit,update アクションを追加

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -11,6 +11,19 @@ class GroupsController < ApplicationController
     else
       render :new
     end
+
+  def edit
+    @group = Group.find(params[:id])
+  end
+
+  def update
+    @group = Group.find(params[:id])
+    if @group.update(group_params)
+      redirect_to root_path, notice: 'グループを更新しました'
+    else
+      render :edit
+  end
+
   end
 
   private
