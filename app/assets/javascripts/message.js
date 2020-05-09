3f11fa2d821d421b9cb7ed0ba5f8ef44c843d618
pregabalin75mg <tsubasa1117@mbr.nifty.com>
2020-05-09 23:47:09 +0900

    add append(html) and val('')

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -2,52 +2,45 @@ $(function(){
       function buildHTML(message){
         if ( message.image ) {
           var html =
-            `<div class="chat-main">
-              <div class="chat-main__massage">
-                <div class="chat-main__massage__user">
-                  <div class="chat-main__massage__user__UserName">
-                    ${message.user_name}
-                  </div>
-                  <div class="class="chat-main__massage__user__date">
-                    ${message.created_at}
-                  </div>
+            `
+              <div class="chat-main__message__user">
+                <div class="chat-main__message__user__UserName">
+                  ${message.user_name}
                 </div>
-                <div class="chat-main__message__UserMessage">
-                  <p div class="chat-main__message__UserMessage__content">
-                    ${message.content}
-                  </p>
+                <div class="chat-main__message__user__date">
+                  ${message.created_at}
                 </div>
-                <img src=${message.image} >
               </div>
-            </div>`
-          return html;
+                <div class="chat-main__message__UserMessage">
+                <p class="chat-main__message__UserMessage__content">
+                  ${message.content}
+                </p>
+                <img src=${message.image} >
+              </div>`
+        return html;
         } else {
           var html =
-            `<div class="chat-main">
-              <div class="chat-main__massage">
-                <div class="chat-main__massage__user">
-                  <div class="chat-main__massage__user__UserName">
-                    ${message.user_name}
-                  </div>
-                  <div class="class="chat-main__massage__user__date">
-                  ${message.created_at}
-                  </div>
+            `
+              <div class="chat-main__message__user">
+                <div class="chat-main__message__user__UserName">
+                  ${message.user_name}
                 </div>
-                <div class="chat-main__message__UserMessage">
-                  <p div class="chat-main__message__UserMessage__content">
-                    ${message.content}
-                  </p>
+                <div class="chat-main__message__user__date">
+                ${message.created_at}
                 </div>
               </div>
-            </div>`
-          return html;
+              <div class="chat-main__message__UserMessage">
+                <p class="chat-main__message__UserMessage__content">
+                  ${message.content}
+                </p>
+              </div>`
+        return html;
           };
         }
   $('#new_message').on('submit',function(e) {
     e.preventDefault();
     var formData = new FormData(this);
     var url = $(this).attr('action');
-    console.log(formData)
     $.ajax({
       url: url,
       type: "POST",
@@ -55,6 +48,11 @@ $(function(){
       dataType: 'json',
       processData: false,
       contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.chat-main__message').append(html)
+      $('#message_content').val('')
     })
   });
 });
