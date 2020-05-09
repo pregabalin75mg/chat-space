44c0faaae3cfea143a8aeaa5e8a10fce36aed536
pregabalin75mg <tsubasa1117@mbr.nifty.com>
2020-05-09 22:35:07 +0900

    create function "buildHTML"

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,48 @@
 $(function(){
+      function buildHTML(message){
+        if ( message.image ) {
+          var html =
+            `<div class="chat-main">
+              <div class="chat-main__massage">
+                <div class="chat-main__massage__user">
+                  <div class="chat-main__massage__user__UserName">
+                    ${message.user_name}
+                  </div>
+                  <div class="class="chat-main__massage__user__date">
+                    ${message.created_at}
+                  </div>
+                </div>
+                <div class="chat-main__message__UserMessage">
+                  <p div class="chat-main__message__UserMessage__content">
+                    ${message.content}
+                  </p>
+                </div>
+                <img src=${message.image} >
+              </div>
+            </div>`
+          return html;
+        } else {
+          var html =
+            `<div class="chat-main">
+              <div class="chat-main__massage">
+                <div class="chat-main__massage__user">
+                  <div class="chat-main__massage__user__UserName">
+                    ${message.user_name}
+                  </div>
+                  <div class="class="chat-main__massage__user__date">
+                  ${message.created_at}
+                  </div>
+                </div>
+                <div class="chat-main__message__UserMessage">
+                  <p div class="chat-main__message__UserMessage__content">
+                    ${message.content}
+                  </p>
+                </div>
+              </div>
+            </div>`
+          return html;
+          };
+        }
   $('#new_message').on('submit',function(e) {
     e.preventDefault();
     var formData = new FormData(this);
