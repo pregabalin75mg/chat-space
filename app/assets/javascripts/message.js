6d6ec9aa1c56eed60e5c05e5d8117a399b6a1f19
pregabalin75mg <tsubasa1117@mbr.nifty.com>
2020-05-10 12:24:54 +0900

    Delete " ; " in fail_function

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -56,7 +56,7 @@ $(function(){
       $('form')[0].reset();
     })
     .fail(function() {
-      alert("メッセージ送信に失敗しました");
+      alert("メッセージ送信に失敗しました")
     });
     .always(function() {
       $('.chat-main__form__submit-btn').prop('disabled', false);
