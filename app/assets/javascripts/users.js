c62fcecf1981811648dfd8f1c6fdbdc8c4b07f1b
pregabalin75mg <tsubasa1117@mbr.nifty.com>
2020-05-10 16:35:52 +0900

    add forEach

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,4 +1,5 @@
 $(function(){
+  
   $("#user-search-field").on("keyup", function(){
     let input = $("#user-search-field").val();
     $.ajax({
@@ -8,10 +9,20 @@ $(function(){
       data: { keyword: input}
     })
     .done(function(users) {
-      console.log("成功")
+      $("#user-search-result").empty();
+
+      if (users.length !== 0) {
+        users.forEach(function(user) {
+          addUser(user);
+        });
+      } else if (input == 0) {
+        return false;
+      } else {
+        addNoUser();
+      }
     })
     .fail(function(){
-      console.log("失敗")
-    });
+      alert("ユーザー表示できませんでした。");
+    })
   });
 });
